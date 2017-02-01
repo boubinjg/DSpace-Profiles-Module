f37d11531481c9b1e43f3a461ed8071a2ed0a143
boubinjg <boubinjg@gmail.com>
2017-02-01 00:19:13 +0000

    restart

--- copy.sh
+++ copy.sh
@@ -1,3 +1,4 @@
+#copies the profiels module to this directory and pushes it to github
 cp ../DSpace/dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser/Profiles.java .
 git add *
 git commit -m "copy.sh commit"
