1ee6478008740d49c01d4aca440db13b89b1a0e9
boubinjg <boubinjg@gmail.com>
2017-02-23 03:25:45 +0000

    postgres error

--- Profiles.java
+++ Profiles.java
@@ -522,7 +522,7 @@ public class Profiles extends AbstractDSpaceTransformer {
 				formLink = request.getParameter("linkedin"),
 				formResGate = request.getParameter("research gate"),
 				formTwitter = request.getParameter("twitter");
-			
+			//getting postgres error 42601 SYNTAX ERROR, find out which line is causing the error	
 			try {
 			
 			Connection conn = null;
