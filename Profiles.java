3bd845b222326bdae116bc49fb68808b587ad63b
boubinjg <boubinjg@gmail.com>
2017-03-06 22:26:50 +0000

    module form

--- Profiles.java
+++ Profiles.java
@@ -154,11 +154,12 @@ public class Profiles extends AbstractDSpaceTransformer {
 	 * Add some basic contents
 	 */
 	
-	Text getText(List form, String textTitle, static final Message textLabel, int size) {
+	public Text getText(List form, String textTitle, Message textLabel, int size) throws WingException {
 		Text t = form.addItem().addText(textTitle);
 		t.setLabel(textLabel);
 		t.setValue(parameters.getParameter(textTitle, ""));
 		t.setSize(0, size);
+		return t;
 	}
 
 	public void addBody(Body body) throws SAXException, WingException {
@@ -336,122 +337,30 @@ public class Profiles extends AbstractDSpaceTransformer {
 
 			List form = formDiv.addList("form", List.TYPE_FORM);
 
-			/*Text fname = form.addItem().addText("name");
-			fname.setLabel(F_name);
-			fname.setValue(parameters.getParameter("name", ""));
-			fname.setSize(0,50);	*/
 			Text fname = getText(form, "name", F_name, 50);		
+			Text fPictureURL = getTExt(form, "Picture URL", F_picurl, 100);			
+			Text fJobTitle = getText(form, "job title", F_jobTitle, 100);
+			Text fResearch = getText(form, "research", F_research, 100);
+			Text fAddress = getText(form,"address", F_address, 50);
+			Text fPhone = getText(form, "phone", F_phone, 12);
+			Text fEmail = getText(form, "email", F_email, 50);
+			Text fWebsite = getText(form, "website", F_website, 100);
+			Text fDegree = getText(form, "degree", F_degree, 100);
+			Text fEarnedFrom = getText(form, "earned from", F_earnedFrom, 100);
+			Text fDatesAttended = getText(form, "dates attended", F_datesAttended, 50);
+			Text fOrganization = getText(form, "organization", F_organization, 100);
+			Text fOrgJobTitle = getText(form, "forg job title", F_orgJobTitle, 100);
+			Text fdateWorked = getText(form, "date worked", F_orgJobTitle, 50);
+			Text fGrantTitle = getText(form, "grant title", F_grantTitle, 100);
+			Text fGrantLength = getText(form, "grant length", F_grantLength, 50);		
+			Text fGrantNumber = getText(form, "grant number", F_grantNumber, 50);
+			Text fOrcidURL = getText(form, "orcid", F_orcidURL,100);
+			Text fAcademiaURL = getText(form, "academia", F_academiaURL, 100);
+			Text fGooglePlusURL = getText(form, "google plus", F_googleplusURL, 100);
+			Text fLinkedinURL = getText(form, "linkedin",F_linkedinURL, 100);
+			Text fResearchGateURL = getText(form, "research gate", F_researchgateURL, 100);
+			Text fTwitterURL = getText(form, "twitter", F_twitterURL, 100);			
 
-			Text fPictureURL = form.addItem().addText("Picture URL");
-			fPictureURL.setLabel(F_picurl);
-			fPictureURL.setValue(parameters.getParameter("Picture URL", ""));
-			fPictureURL.setSize(0,100);
-
-			Text fJobTitle = form.addItem().addText("job title");
-			fJobTitle.setLabel(F_jobTitle);
-			fJobTitle.setValue(parameters.getParameter("job title", ""));
-			fJobTitle.setSize(0,100);
-
-			Text fResearch = form.addItem().addText("research");
-			fResearch.setLabel(F_research);
-			fResearch.setValue(parameters.getParameter("research", ""));
-			fResearch.setSize(0,100);
-
-			Text fAddress = form.addItem().addText("address");
-			fAddress.setLabel(F_address);
-			fAddress.setValue(parameters.getParameter("address", ""));
-			fAddress.setSize(0,50);
-
-			Text fPhone = form.addItem().addText("phone");
-			fPhone.setLabel(F_phone);
-			fPhone.setValue(parameters.getParameter("phone", ""));
-			fPhone.setSize(0,12);
-
-			Text fEmail = form.addItem().addText("email");
-			fEmail.setLabel(F_email);
-			fEmail.setValue(parameters.getParameter("email", ""));
-			fEmail.setSize(0,50);
-
-			Text fWebsite = form.addItem().addText("website");
-			fWebsite.setLabel(F_website);
-			fWebsite.setValue(parameters.getParameter("website", ""));
-			fWebsite.setSize(0,100);
-
-			Text fDegree = form.addItem().addText("degree");
-			fDegree.setLabel(F_degree);
-			fDegree.setValue(parameters.getParameter("degree", ""));
-			fDegree.setSize(0,100);
-
-			Text fEarnedFrom = form.addItem().addText("earned from");
-			fEarnedFrom.setLabel(F_earnedFrom);
-			fEarnedFrom.setValue(parameters.getParameter("earned from", ""));
-			fEarnedFrom.setSize(0,100);
-
-			Text fDatesAttended = form.addItem().addText("dates attended");
-			fDatesAttended.setLabel(F_datesAttended);
-			fDatesAttended.setValue(parameters.getParameter("dates attended", ""));
-			fDatesAttended.setSize(0,50);
-
-			Text fOrganization = form.addItem().addText("organization");
-			fOrganization.setLabel(F_organization);
-			fOrganization.setValue(parameters.getParameter("organization", ""));
-			fOrganization.setSize(0,100);
-
-			Text fOrgJobTitle = form.addItem().addText("forg job title");
-			fOrgJobTitle.setLabel(F_orgJobTitle);
-			fOrgJobTitle.setValue(parameters.getParameter("forg job title", ""));
-			fOrgJobTitle.setSize(0,100);
-
-			Text fdateWorked = form.addItem().addText("date worked");
-			fdateWorked.setLabel(F_dateWorked);
-			fdateWorked.setValue(parameters.getParameter("date worked", ""));
-			fdateWorked.setSize(0,50);
-
-			Text fGrantTitle = form.addItem().addText("grant title");
-			fGrantTitle.setLabel(F_grantTitle);
-			fGrantTitle.setValue(parameters.getParameter("grant title", ""));
-			fGrantTitle.setSize(0,100);
-
-			Text fGrantLength = form.addItem().addText("grant length");
-			fGrantLength.setLabel(F_grantLength);
-			fGrantLength.setValue(parameters.getParameter("grant length", ""));
-			fGrantLength.setSize(0,50);
-
-			Text fGrantNumber = form.addItem().addText("grant number");
-			fGrantNumber.setLabel(F_grantNumber);
-			fGrantNumber.setValue(parameters.getParameter("grant number", ""));
-			fGrantNumber.setSize(0,50);
-
-			Text fOrcidURL = form.addItem().addText("orcid");
-			fOrcidURL.setLabel(F_orcidURL);
-			fOrcidURL.setValue(parameters.getParameter("orcid", ""));
-			fOrcidURL.setSize(0,100);
-
-			Text fAcademiaURL = form.addItem().addText("academia");
-			fAcademiaURL.setLabel(F_academiaURL);
-			fAcademiaURL.setValue(parameters.getParameter("academia", ""));
-			fAcademiaURL.setSize(0,100);
-
-			Text fGooglePlusURL = form.addItem().addText("google plus");
-			fGooglePlusURL.setLabel(F_googleplusURL);
-			fGooglePlusURL.setValue(parameters.getParameter("google plus", ""));
-			fGooglePlusURL.setSize(0,100);
-
-			Text fLinkedinURL = form.addItem().addText("linkedin");
-			fLinkedinURL.setLabel(F_linkedinURL);
-			fLinkedinURL.setValue(parameters.getParameter("linkedin", ""));
-			fLinkedinURL.setSize(0,100);
-
-			Text fResearchGateURL = form.addItem().addText("research gate");
-			fResearchGateURL.setLabel(F_researchgateURL);
-			fResearchGateURL.setValue(parameters.getParameter("research gate", ""));
-			fResearchGateURL.setSize(0,100);
-
-			Text fTwitterURL = form.addItem().addText("twitter");
-			fTwitterURL.setLabel(F_twitterURL);
-			fTwitterURL.setValue(parameters.getParameter("twitter", ""));
-			fTwitterURL.setSize(0,100);
-			
 			form.addItem().addHidden("isSent").setValue("true");
 			form.addItem().addButton("submit").setValue("Submit");
 
