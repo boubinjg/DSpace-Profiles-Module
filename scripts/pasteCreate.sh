77e856b7ab2dafef3b34f8335e7b02ad9bf2e842
boubinjg <boubinjg@gmail.com>
2017-03-31 17:32:15 +0000

    added manager

--- scripts/pasteCreate.sh
+++
@@ -1,2 +0,0 @@
-#pastes profiles into its correct directory
-cp ../ProfilesCreate.java ~/DSpace/dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser
---
+++ scripts/pasteManager.sh
@@ -0,0 +1,2 @@
+#pastes profiles into its correct directory
+cp ../ProfileManager.java ~/DSpace/dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser
