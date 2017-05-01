23152993b81398327b70082644ce5a381dd4f0ab
boubinjg <boubinjg@gmail.com>
2017-05-01 17:44:21 +0000

    install

--- install/dspaceProfileModuleInstall.sh
+++ install/dspaceProfileModuleInstall.sh
@@ -13,12 +13,19 @@ cp lib/ProfileManager.java $value/dspace-xmlui/src/main/java/org/dspace/app/xmlu
 echo "installing profile homepage"
 cp lib/ProfilesHome.java $value/dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser
 echo "updating wing Division class"
-cp lib/Division.java $value/dspace-xmlui/src/main/java/org/dspace/app/xmlui/wing
+cp lib/Division.java $value/dspace-xmlui/src/main/java/org/dspace/app/xmlui/wing/element
 #install CSS
 echo "installing CSS"
 cp lib/moduleCSS.css $value/dspace-xmlui/src/main/webapp/themes/Mirage/lib/css
 echo "installing Javascript"
 cp lib/moduleJS.js $value/dspace-xmlui/src/main/webapp/themes/Mirage/lib/js
+echo "installing images"
+cp lib/academia-logo.png $value/dspace-xmlui/src/main/webapp/themes/Mirage/images
+cp lib/orcid-logo.png $value/dspace-xmlui/src/main/webapp/themes/Mirage/images
+cp lib/researchgate-logo.png $value/dspace-xmlui/src/main/webapp/themes/Mirage/images
+cp lib/linkedin-logo.png $value/dspace-xmlui/src/main/webapp/themes/Mirage/images
+cp lib/google-plus-logo.png $value/dspace-xmlui/src/main/webapp/themes/Mirage/images
+cp lib/twitter-logo.png $value/dspace-xmlui/src/main/webapp/themes/Mirage/images
 echo "===================================="
 echo "Done Installing Changes"
 echo "Please Recompile and Restart DSpace"
